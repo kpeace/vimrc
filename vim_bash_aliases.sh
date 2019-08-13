be5f1c254ef602226d719feaf9d390cb3a76a845
Kramer Peace <kpeace1@gmail.com>
2019-08-13 14:30:12 +0300

    Use nvim when possible

--- vim_bash_aliases.sh
+++ vim_bash_aliases.sh
@@ -1,2 +1,6 @@
-alias vide="vim -u ~/.vim/vide.vimrc"
+if hash nvim 2>/dev/null; then
+	alias vide="nvim -u ~/.vim/vide.vimrc"
+else
+	alias vide="vim -u ~/.vim/vide.vimrc"
+fi
 alias init_vide="~/.vim/init_vim_ide_project.sh"
